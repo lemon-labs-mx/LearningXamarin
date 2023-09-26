1cb23b4b258864c25370b816a17b0ac9756b8647
Brandon Sandoval <colima_97@hotmail.com>
2023-09-26 11:13:20 -0600

    [Feature] - Create API Client Service
    Also, created its first method "GetProducts"

---
+++ LearningXamarin/LearningXamarin/Models/Responses/StoreProductResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace LearningXamarin.Models.Responses
+{
+	public class StoreProductResponse
+	{
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+        public Rating Rating { get; set; }
+    }
+
+    public class Rating
+    {
+        public double Rate { get; set; }
+        public int Count { get; set; }
+    }
+}
+
---
+++ LearningXamarin/LearningXamarin/Services/APIClientService/APIClientService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LearningXamarin.Models.Responses;
+using RestSharp;
+
+namespace LearningXamarin.Services.APIClientService
+{
+    public class APIClientService
+	{
+		public const string URLBase = "https://fakestoreapi.com/";
+		public RestClient client;
+
+		public APIClientService()
+		{
+            var options = new RestClientOptions(URLBase);
+            client = new RestClient(options);
+		}
+
+		public async Task<RestResponse<List<StoreProductResponse>>> GetProducts()
+		{
+            var request = new RestRequest("products");
+            var restResponse = await client.ExecuteGetAsync<List<StoreProductResponse>>(request);
+
+			return restResponse;
+        }
+    }
+}
