1ca339989ac3b12323dc429f4dd9055178590a9a
Brandon Sandoval <colima_97@hotmail.com>
2023-09-27 09:38:37 -0600

    [WIP] - Create Colors ResourceDictionary file
    Also, add some style in every page

--- LearningXamarin/LearningXamarin/App.xaml.cs
+++ LearningXamarin/LearningXamarin/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using LearningXamarin.Views;
+﻿using System;
+using LearningXamarin.Styles;
+using LearningXamarin.Views;
 using Xamarin.Forms;
 
 namespace LearningXamarin
@@ -8,8 +10,14 @@ namespace LearningXamarin
         public App ()
         {
             InitializeComponent();
+            ApplyAppResources();
 
             MainPage = new NavigationPage(new LoginPage());
+        }
+
+        private void ApplyAppResources()
+        {
+            Resources.MergedDictionaries.Add(new Colors());
         }
 
         protected override void OnStart ()
