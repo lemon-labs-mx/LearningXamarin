6c3023b228e997c8783d3e0686ac49125f99389b
Brandon Sandoval <colima_97@hotmail.com>
2023-09-28 11:46:57 -0600

    [WIP] - Add FontAwesome files

--- LearningXamarin/LearningXamarin/AssemblyInfo.cs
+++ LearningXamarin/LearningXamarin/AssemblyInfo.cs
@@ -1,3 +1,7 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("fa-regular-400.ttf", Alias = "FARegular")]
+[assembly: ExportFont("fa-solid-900.ttf", Alias = "FASolid")]
---
+++ LearningXamarin/LearningXamarin/Helpers/Constants/IconConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace LearningXamarin.Helpers.Constants
+{
+	public class IconConstants
+	{
+		public const string SearchIcon = "\uf002";
+    }
+}
