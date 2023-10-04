c7b6ad5a5d4198ed7c381b8e29e29bd557958c5d
Brandon Sandoval <colima_97@hotmail.com>
2023-10-04 12:04:30 -0600

    [WIP] - Add Popup NuGet

--- LearningXamarin/LearningXamarin.iOS/AppDelegate.cs
+++ LearningXamarin/LearningXamarin.iOS/AppDelegate.cs
@@ -23,6 +23,8 @@ namespace LearningXamarin.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Rg.Plugins.Popup.Popup.Init();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
