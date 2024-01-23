7f8a2d72371566ff43eed6e422e957726408a68f
Brandon Sandoval <colima_97@hotmail.com>
2024-01-22 22:23:07 -0600

    [WIP] - Create app shell

---
+++ LearningXamarin/LearningXamarin/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace LearningXamarin
+{    
+    public partial class AppShell : Shell
+    {    
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
