3754f1413badf02a6ddc3fded64318c720f62ea7
Brandon Sandoval <colima_97@hotmail.com>
2024-01-22 22:36:36 -0600

    [WIP] - Add the query attributable interface to base vm

--- LearningXamarin/LearningXamarin/ViewModels/BaseViewModel.cs
+++ LearningXamarin/LearningXamarin/ViewModels/BaseViewModel.cs
@@ -1,9 +1,10 @@
-﻿using Xamarin.Essentials;
+﻿using System.Collections.Generic;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace LearningXamarin.ViewModels
 {
-	public class BaseViewModel : BindableObject
+	public class BaseViewModel : BindableObject, IQueryAttributable
 	{
 		private bool _isBusy;
 
@@ -15,21 +16,28 @@ namespace LearningXamarin.ViewModels
 				if (value == _isBusy)
 				{
 					return;
-                }
+				}
 
 				_isBusy = value;
 				OnPropertyChanged(nameof(IsBusy));
-            }
-        }
+			}
+		}
 
 		//L1 - Add a property to check if the user has internet connection
 		//using Xamarin.Essentials
 		public bool HasInternetConnection => IsNetworkConnected();
 
-        private bool IsNetworkConnected()
-        {
+		//L3 - Add IQueryAttributable to pass data using Shell
+		//Set this method as public virtual to enable overwritting
+		//in your own view models
+		public virtual void ApplyQueryAttributes(IDictionary<string, string> query)
+		{
+		}
+
+		private bool IsNetworkConnected()
+		{
 			return Connectivity.NetworkAccess == NetworkAccess.Internet;
-        }
+		}
 	}
 }
 
