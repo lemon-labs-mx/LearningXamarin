f31c509900824eb3f4a451ab6c89e342044112d1
Brandon Sandoval <colima_97@hotmail.com>
2024-01-24 11:09:09 -0600

    [WIP] - Create navigation service

---
+++ LearningXamarin/LearningXamarin/Services/NavigationService/NavigationService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using LearningXamarin.ViewModels;
+using Xamarin.Forms;
+
+namespace LearningXamarin.Services.NavigationService
+{
+    public class NavigationService
+    {
+        public async Task NavigateTo(string route)
+        {
+            await Shell.Current.GoToAsync($"/{route}");
+        }
+
+        public async Task NavigateTo<T>(string route, T param)
+        {
+            await Shell.Current.GoToAsync($"/{route}");
+
+            if (Shell.Current.CurrentPage == null)
+            {
+                //Await to shell to load the page
+                await Task.Delay(100);
+            }
+
+            var currentPage = Shell.Current.CurrentPage;
+
+            if (currentPage != null
+                && currentPage.BindingContext is BaseViewModel bvm)
+            {
+                bvm.OnNavigating(param);
+            }
+        }
+    }
+}
+
