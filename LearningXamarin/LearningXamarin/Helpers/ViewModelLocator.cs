5352bc2b98d461ecb130076facc2d3dd815137e4
Brandon Sandoval <colima_97@hotmail.com>
2024-01-30 10:54:56 -0600

    [Update] - Remove example method extension

--- LearningXamarin/LearningXamarin/Helpers/ViewModelLocator.cs
+++ LearningXamarin/LearningXamarin/Helpers/ViewModelLocator.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace LearningXamarin.Helpers
 {
@@ -7,22 +6,12 @@ namespace LearningXamarin.Helpers
     {
         private static readonly ServiceLocator Container;
 
-        //static ViewModelLocator() => Container = new ServiceLocator();
-        static ViewModelLocator()
-        {
-            Container = new ServiceLocator();
-        }
+        static ViewModelLocator() => Container = new ServiceLocator();
 
         public static TViewModel GetViewModel<TViewModel>(this ContentPage _)
         {
             TViewModel viewModel = Container.Resolve<TViewModel>();
             return viewModel;
-        }
-
-        //Other example
-        public static string ToTitleCase(this string str)
-        {
-            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
         }
     }
 }
